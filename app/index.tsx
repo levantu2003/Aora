5b61d522e33301382d7ad4c3a28a6c0a9ae913f5
levantu2003 <levantu18071970@gmail.com>
2024-07-27 19:28:10 +0700

    Finish Onboarding Screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,56 @@
+import { Image, ScrollView, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { images } from "../constants";
+import CustomButton from "@/components/CustomButton";
 import { StatusBar } from "expo-status-bar";
-import { Text, View } from "react-native";
-import { Link } from "expo-router";
+import { router } from "expo-router";
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-3xl font-cfbold">Aura!</Text>
-      <StatusBar style="auto" />
-      <Link href="/home" style={{ color: "blue" }}>
-        Go to home
-      </Link>
+    <View>
+      <SafeAreaView className="bg-primary h-full">
+        <ScrollView
+          contentContainerStyle={{
+            height: "100%",
+          }}
+        >
+          <View className="w-full justify-center items-center min-h-[85vh] px-4">
+            <Image
+              source={images.logo}
+              className="w-[130px] h-[84px]"
+              resizeMode="contain"
+            />
+            <Image
+              source={images.cards}
+              className="max-w-[380px] w-full h-[300px]"
+              resizeMode="contain"
+            />
+            <View className="relative mt-5">
+              <Text className="text-3xl text-white font-bold text-center">
+                Khám phá những khả năng vô tận với {""}
+                <Text className="text-secondary-200">Aora</Text>
+              </Text>
+              <Image
+                source={images.path}
+                className="w-[240px] h-[10px] absolute -bottom-2 -right-1"
+                resizeMode="contain"
+              />
+            </View>
+            <Text className="text-sm font-cfregular text-gray-100 mt-7 text-center">
+              Nơi sáng tạo gặp đổi mới: bắt tay vào hành trình khám phá không
+              giới hạn cùng Aura
+            </Text>
+            <CustomButton
+              title="Tiếp tục với Email"
+              handlePress={() => router.push("/sign-in")}
+              containerStyles="w-full mt-7"
+              textStyles=""
+              isLoading={false}
+            />
+          </View>
+        </ScrollView>
+        <StatusBar backgroundColor="#161622" style="light" />
+      </SafeAreaView>
     </View>
   );
 }
---
+++ components/CustomButton.tsx
@@ -0,0 +1,33 @@
+import { TouchableOpacity, Text } from "react-native";
+import React from "react";
+
+const CustomButton = ({
+  title,
+  handlePress,
+  containerStyles,
+  textStyles,
+  isLoading,
+}: {
+  title: any;
+  handlePress: any;
+  containerStyles: any;
+  textStyles: any;
+  isLoading: boolean;
+}) => {
+  return (
+    <TouchableOpacity
+      onPress={handlePress}
+      activeOpacity={0.7}
+      className={`bg-secondary rounded-xl min-h-[62px] justify-center items-center ${containerStyles} ${
+        isLoading ? "opacity-50" : ""
+      } `}
+      disabled={isLoading}
+    >
+      <Text className={`text-primary font-cfbold text-lg ${textStyles}`}>
+        {title}
+      </Text>
+    </TouchableOpacity>
+  );
+};
+
+export default CustomButton;
